c8a09c7a6c598c0ec67cb636d755dbb4ca0fc7dd
Bhushan Patil <bhupatil001@gmail.com>
2021-04-16 18:46:17 +0530

    ADDED: first demo sprigboot project configuration using eclipse

---
+++ MyFirstAppSpring/src/main/java/com/myfirstapp/MyFirstAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.myfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppSpringApplication.class, args);
+	}
+
+}
